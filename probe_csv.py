ca0226833a4657a8d08dc50179fe50aef9292ab8
Vladyas <yaskevich@mail.ru>
2022-05-24 17:31:55 +0300

    Read Write CSV (probe_csv.py)

---
+++ probe_csv.py
@@ -0,0 +1,35 @@
+import csv
+FILE_UNIQ_NUMBERS = 'uniq_numbers.csv'
+def get_uniq_numbers(file):
+    result = set()
+    try:
+        f = open(file)
+        with f:
+            reader = csv.reader(f)
+            for row in reader:
+                result |= set(map(lambda x:x.strip(), row))
+    except IOError as e:
+        print(u'Чтение уникальных номеров:не удалось открыть файл с уникальными номерами')
+    return result
+
+def update_uniq_numbers(file, numbers):
+    result = set()
+    try:
+        f = open(file)
+        with f:
+            reader = csv.reader(f)
+            for row in reader:
+                result |= set(map(lambda x: x.strip(), row))
+    except IOError as e:
+        print(u'Обновление уникальных номеров:Не удалось открыть файл с уникальными номерами')
+
+    result |=numbers
+    result = ([x] for x in sorted(result))
+
+    with open(file, 'w', newline='') as f:
+        writer = csv.writer(f, dialect='excel')
+        writer.writerows(result)
+
+if __name__ == '__main__':
+    print(get_uniq_numbers(FILE_UNIQ_NUMBERS))
+    update_uniq_numbers(FILE_UNIQ_NUMBERS, {'A1', 'A2'})
